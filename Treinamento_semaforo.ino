0919d79a038a46ec7bb93bf77591d8b6b098d8e7
LeonardoMaximoSilva <leomax2002@gmail.com>
2022-03-18 18:39:14 -0300

    Semaforo

--- Treinamento_semaforo.ino
+++ Treinamento_semaforo.ino
@@ -35,28 +35,26 @@ void loop() {
   intervalo_botao = tempo_at - tempo_botao;
   ped = digitalRead(pin_button);
   if(ped == HIGH || (botao_acionado != 0 && intervalo_botao < 8000)){
-    if(intervalo >= 0 && intervalo < 2000){
+    if(intervalo < 2000){
       digitalWrite(pin_y,LOW);
       digitalWrite(pin_r,HIGH);
       digitalWrite(pin_rped,LOW);
       digitalWrite(pin_gped,HIGH);
       }
-    else if (intervalo >= 2000 && intervalo < 5000){
+    else if (intervalo < 5000){
       digitalWrite(pin_r,LOW);
       digitalWrite(pin_g, HIGH);
       digitalWrite(pin_gped,LOW);
       digitalWrite(pin_rped,HIGH);
-      if(intervalo >= 4000){
-        delay(200);
-        digitalWrite(pin_rped,LOW);
-        delay(200);
-        }
       }
-    else if (intervalo >= 3000 && intervalo < 6000){
+    else if (intervalo < 6000){
       digitalWrite(pin_g,LOW);
       digitalWrite(pin_y, HIGH);
       digitalWrite(pin_gped,LOW);
       digitalWrite(pin_rped,HIGH);
+      delay(200);
+      digitalWrite(pin_rped,LOW);
+      delay(200);
       }
     else{
       tempo_prev = millis();
